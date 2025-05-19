f889ca4cc08a27e608388a1cfb2ddf73be8d79cc
LiorYosef25 <lior@impact-networks.co.il>
2025-05-19 20:29:48 +0300

    Delete la_inventory_dashboard directory

--- la_inventory_dashboard/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="he" dir="rtl">
-      <body className="p-6 bg-white text-gray-800 font-sans">{children}</body>
-    </html>
-  );
-}
